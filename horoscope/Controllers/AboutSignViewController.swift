b07fc170d2dbb5c602dd363e73fe131c09a7d34e
Panin <panin1970@gmail.com>
2022-01-08 21:21:54 +0300

    added controllers and outlets

---
+++ horoscope/Controllers/AboutSignViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AboutSignViewController.swift
+//  horoscope
+//
+//  Created by Александр Панин on 08.01.2022.
+//
+
+import UIKit
+
+class AboutSignViewController: UIViewController {
+
+    @IBOutlet weak var signImageView: UIImageView!
+    @IBOutlet weak var shortLabel: UILabel!
+    @IBOutlet weak var fullLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ horoscope/Controllers/MoreSignViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MoreSignViewController.swift
+//  horoscope
+//
+//  Created by Александр Панин on 08.01.2022.
+//
+
+import UIKit
+
+class MoreSignViewController: UIViewController {
+
+    @IBOutlet weak var signImageView: UIImageView!
+    @IBOutlet weak var forecastLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
